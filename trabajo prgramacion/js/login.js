1ecd9d73bb4e4a214f2db34b15a0092d2621b268
josefina niell <josefinaniell@MacBook-Air-de-josefina-7.local>
2024-11-21 13:36:34 -0300

    javalogin

---
+++ trabajo prgramacion/js/login.js
@@ -0,0 +1,28 @@
+
+const formulario = document.querySelector(".acceso");
+
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  const email = document.querySelector(".username").value.trim();
+  const contraseña = document.querySelector(".contraseña").value.trim();
+
+  if (email === "") {
+    alert("Por favor complete el campo email"); 
+    return;  
+  }
+  if (contraseña === "") {
+    alert("Por favor complete el campo contraseña"); 
+    return; 
+  }
+  window.location.href = "./index.html"; 
+});
+
+// primero hay que elegir el formulario que puse en el html
+// hay que evitar que el formulario se mande directo por si no completo bien todo 
+ // usamos lo que puso en contraeña y en usuario
+// Ver si lo completo 
+// si no lo completa avisarle 
+// frenar para que no pueda avanzar a la otra pagina 
+ // repetir los pasos del email  
+ // Si todo está bien,cambiamos de página
