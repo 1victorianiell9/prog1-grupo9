a35c845f686045da73ae55347bacae320a6030ac
delfina33-ai <dleers@udesa.edu.ar>
2024-11-26 15:32:30 -0300

    m

--- trabajo prgramacion/js/validacion.js
+++ trabajo prgramacion/js/validacion.js
@@ -2,13 +2,5 @@ let form = document.querySelector(".busca")
 let input = document.querySelector(".in");
 let value = input.value
 
-form.addEventListener("submit", function(event){
-    if (value == ""){
-        alert(" ");
-        event.preventDefault();
-    }else if (value.length < 3){
-        alert("Menor a 3");
-        event.preventDefault()
-    }
-})
+
 
